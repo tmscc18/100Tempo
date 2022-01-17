7cac95671e40cdbb11db61e32f29b39b2a6c61d0
GuilhermeT <gui.v.torquato@gmail.com>
2022-01-17 14:10:42 +0000

    Ateração ao ficheiro utilizadores.py

--- src/utilizadores.py
+++ src/utilizadores.py
@@ -7,13 +7,11 @@ def cria_novo_utilizador():
 
     :return: dicionario com o novo utilizador, {"nome": <<nome>>, "email": <<email>>}
     """
-    nome = input("NIF do Cliente? ")
-    email = input("Série da Trotinete? ")
-
-
-    print("Data e Hora de aluguer? -> ", hora_data)
-    estado_troti = input("Estado da trotinete? ")
-    return {"nif": nif_cliente, "serie_trotinete": num_serie_troti, "data_hora": hora_data, "estado": estado_troti}
+    nome = input("Nome? ")
+    email = input("Email? ")
+    telemovel = input("Telemovel? ")
+    password = input("Password? ")
+    return {"nome": nome, "email": email, "telemovel": telemovel, "password": password}
 
 
 def imprime_lista_de_utilizadores(lista_de_utilizadores):
